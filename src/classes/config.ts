1286755fd1d6f31f7cb4ebd0a79cfc2c5eaf3d15
Muhammad Jalil <muhammad.jalil@haball.pk>
2025-05-18 13:22:26 +0500

    Refactor config handling by converting Config class to createConfig function for simplicity and clarity

--- src/classes/config.ts
+++ src/classes/config.ts
@@ -1,19 +1,13 @@
-import { ConfigType, validateEnvVariables } from '../utils/validateEnv';
+import { ConfigType, validateEnvVariables } from "../utils/validateEnv";
 
-export class Config<T extends readonly string[]> {
-  private readonly config: Record<T[number], string>;
 
-  constructor(configKeys: T) {
-    this.config = configKeys.reduce(
-      (acc: Record<T[number], string>, key: T[number]) => {
-        acc[key] = process.env[key] || '';
-        return acc;
-      },
-      {} as Record<T[number], string>
-    );
+export function createConfig<T extends readonly string[]>(configKeys: T) {
+  const config = configKeys.reduce((acc: Record<T[number], string>, key: T[number]) => {
+    acc[key] = process.env[key] || "";
+    return acc;
+  }, {} as Record<T[number], string>);
 
-    validateEnvVariables(this.config as unknown as ConfigType);
+  validateEnvVariables(config as unknown as ConfigType);
 
-    Object.freeze(this);
-  }
+  return Object.freeze(config);
 }
