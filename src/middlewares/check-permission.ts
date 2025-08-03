d1657c764e55f8a65b240894d34ea01c25e2d790
unknown <muhammadjalil8481@gmail.com>
2025-08-03 19:54:39 +0500

    added permission middleware

---
+++ src/middlewares/check-permission.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Request, Response } from 'express';
+import { NotAuthorizedError } from '../error-handler';
+import { RedisClientType } from 'redis';
+
+interface PermissionList {
+  roleId: number;
+  permissions: string[];
+}
+
+export function checkPermission(redisClient: RedisClientType, name: string) {
+  return async (req: Request, _res: Response, next: NextFunction) => {
+    let hasPermission = false;
+    const context = 'check-permission.middleware.ts/checkPermission()';
+    const roles = req.currentUser?.roles;
+    if (!roles?.length) throw new NotAuthorizedError('Unauthorized', context);
+
+    let permissions = await redisClient.get('role-permissions');
+    if (!permissions?.length)
+      throw new NotAuthorizedError('Unauthorized', context);
+
+    const parsedPermissions: PermissionList[] = JSON.parse(permissions);
+
+    for (let role of roles) {
+      const filteredPermission = parsedPermissions.find(() => {
+        return roles.includes(role);
+      })?.permissions;
+
+      if (filteredPermission?.includes(name)) {
+        hasPermission = true;
+        break;
+      }
+    }
+    if (hasPermission) next();
+    else throw new NotAuthorizedError('Unauthorized', context);
+  };
+}
