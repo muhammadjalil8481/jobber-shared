50c3d5f1f9a8038c441aa8ca2e6ec7a7e3eb74d5
Muhammad Jalil <muhammad.jalil@haball.pk>
2025-08-04 13:05:21 +0500

    Add consumeMessage function to handle message consumption from RabbitMQ

--- src/events.ts
+++ src/events.ts
@@ -1,4 +1,9 @@
-import client, { Channel, ChannelModel, Options } from 'amqplib';
+import client, {
+  Channel,
+  ChannelModel,
+  ConsumeMessage,
+  Options,
+} from 'amqplib';
 import { StrictLogger } from './logger';
 
 interface CREATE_CONNECTION_PARAMS {
@@ -20,6 +25,18 @@ interface LogParams {
   log: StrictLogger;
   logMessage: string;
   context: string;
+}
+
+interface ConsumerParams {
+  channel: Channel;
+  exchangeName: string;
+  queueName: string;
+  bindingKey: string;
+  handler: (message: ConsumeMessage) => Promise<void>;
+  exchangeType: 'direct' | 'topic' | 'headers' | 'fanout' | 'match';
+  log: StrictLogger;
+  context: string;
+  queueOptions?: Options.AssertQueue;
 }
 
 async function createConnection({
@@ -105,4 +122,56 @@ async function publishDirectMessage({
   }
 }
 
-export { createConnection, publishDirectMessage };
+async function consumeMessage(data: ConsumerParams): Promise<void> {
+  const {
+    channel,
+    exchangeName,
+    queueName,
+    bindingKey,
+    handler,
+    exchangeType,
+    queueOptions = {},
+    log,
+    context,
+  } = data;
+  try {
+    if (!channel) {
+      throw new Error('Channel is not defined');
+    }
+
+    await channel.assertExchange(exchangeName, exchangeType || 'direct');
+    const jobberQueue = await channel.assertQueue(queueName, queueOptions);
+    await channel.bindQueue(jobberQueue.queue, exchangeName, bindingKey);
+
+    channel.consume(
+      jobberQueue.queue,
+
+      async (message: ConsumeMessage | null) => {
+        try {
+          await handler(message!);
+          channel.ack(message!);
+          log.info(
+            `Message successfully process and acknowledged - exhcange ${exchangeName}`,
+            context
+          );
+        } catch (error) {
+          log.error(
+            `Failed to consume message - exchange ${data.exchangeName}`,
+            context
+          );
+          channel.nack(message!, false, true);
+        }
+      },
+      {
+        noAck: true, // Must manually ack or nack message if set to false
+      }
+    );
+  } catch (error) {
+    log.error(
+      `Failed to consume message - exchange ${data.exchangeName}`,
+      context
+    );
+  }
+}
+
+export { createConnection, publishDirectMessage, consumeMessage };
